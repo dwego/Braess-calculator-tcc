7ba52dd65e4acc8bb436a505dc698610eeaf1523
Diego Araujo <dweg0epic@outlook.com>
2026-08-03 19:43:54 -0300

    docs: add protocol layout and refactor config output

--- maps/config.py
+++ maps/config.py
@@ -2,6 +2,6 @@ CENTER_POINT = (-23.1791, -45.8869)  # CENTRO DE SJC
 DIST_M = 1800
 BETWEENNESS_K = 300
 
-GPKG_PATH = "./graph_centro_sjc.gpkg"
-GRAPHML_PATH = "./graph_centro_sjc.graphml"
-PNG_PATH = "./graph_centro_sjc_bc.png"
+GPKG_PATH = "../outputs/graph_centro_sjc.gpkg"
+GRAPHML_PATH = "../outputs/graph_centro_sjc.graphml"
+PNG_PATH = "../outputs/graph_centro_sjc_bc.png"
